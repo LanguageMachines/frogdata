34b03d5af0e7fbea154fe951c4c7f26520dfced1
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-04-09 15:09:35 +0200

    added new example

---
+++ scripts/train_new_ner.sh
@@ -0,0 +1,9 @@
+#! /bin/sh -x
+
+if [ "$exe_bin" = "" ];
+then echo "exe_bin not set";
+     exit;
+fi
+
+cd ../data
+$exe_bin/nergen -O NERDATA -c $exe_bin/../share/frog/nld/frog.cfg --gazeteer=$exe_bin/../share/frog/nld/ners.known ner.data

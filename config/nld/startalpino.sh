244f41fcaed1feaaa86d26340483745ecf18109d
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2019-09-30 17:22:54 +0200

    added a script to start an Alpino Server

---
+++ config/nld/startalpino.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+export PORT=8004
+export TIMEOUT=60000
+export MEMLIMIT=1500M
+export TMPDIR=/tmp
+
+export ALPINO_HOME=/home/sloot/Alpino
+
+## this one is for parsing
+
+PROLOGMAXSIZE=${MEMLIMIT} ${ALPINO_HOME}/bin/Alpino -notk -fast user_max=${TIMEOUT}\
+ server_kind=parse\
+ server_port=${PORT}\
+ assume_input_is_tokenized=on\
+ debug=1\
+ end_hook=xml\
+ -init_dict_p\
+ batch_command=alpino_server 2> ${TMPDIR}/alpino_parse_server.log &
+
+echo "Alpino started"
